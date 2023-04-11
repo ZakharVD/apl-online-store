61e7477e085b7775f903e9f95611e7327ec7f7ec
Zakhar <zakharmac@MacBook-Air-3.local>
2023-04-11 00:15:03 -0400

    fix

--- src/components/category-preview/CategoryPreview.jsx
+++ src/components/category-preview/CategoryPreview.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const CategoryPreview = ({title}) => {
     return (
-        <div className="bg-lightgrey h-[25%] flex justify-left items-center m-[8px]">
-            <Link to={title} className="text-xl uppercase font-medium ml-[10px]">{title}</Link>
-        </div>
+        <Link to={title} className="bg-lightgrey h-[25%] flex justify-left items-center m-[8px]">
+            <span className="text-xl uppercase font-medium ml-[10px]">{title}</span>
+        </Link>
     )
 }
 
