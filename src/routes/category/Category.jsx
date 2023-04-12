73a66e11fdb40a7a745bf7b759db30a4accf5d68
Zakhar <zakharmac@MacBook-Air-3.local>
2023-04-12 15:39:18 -0400

    search bar implemented

--- src/routes/category/Category.jsx
+++ src/routes/category/Category.jsx
@@ -1,51 +1,52 @@
 import { useParams } from "react-router-dom";
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import ProductCard from "../../components/product-card/ProductCard";
 
 const Category = ({ productsToDisplay }) => {
   const { category } = useParams();
-
-  // let finalProducts;
-  // productsToDisplay.map((prod) => {
-  //   if (prod.title === category) {
-  //     finalProducts = prod;
-  //   }
-  //   return finalProducts;
-  // });
-  // console.log(finalProducts.items);
+  console.log("component rendered");
 
   let finalProducts;
   productsToDisplay.map((prod) => {
     if (prod.title === category) {
-      finalProducts = prod; 
+      finalProducts = prod;
     }
     return finalProducts;
   });
-  console.log("final prod-----", finalProducts);
+  const [searchField, setSearchField] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState(finalProducts.items);
 
-  // const [pd, setPd] = useState(finalProducts);
+  const onChangeHandler = (event) => {
+    setSearchField(event.target.value.toLowerCase());
+  };
 
-  // useEffect(() => {
-  //   setPd(test());
-  // }, [category, productsToDisplay]);
-
-  // const products = productsToDisplay[category]; 
-  // console.log('final', products)
-  // // const [products, setProducts] = useState(productsToDisplay[category]);
-
-  // // useEffect(() => {
-  // //   setProducts(productsToDisplay[category]);
-  // // }, [category, productsToDisplay]);
-
-  // console.log(products)
+  useEffect(() => {
+    const newFilteredProducts = finalProducts.items.filter((pd) =>
+      pd.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    setFilteredProducts(newFilteredProducts);
+  }, [finalProducts, searchField]);
 
   return (
-    <div className="flex flex-row justify-center flex-wrap max-w-[700px] m-auto">
-      {finalProducts &&
-        finalProducts.items.map((product) => (
-          <ProductCard key={finalProducts.id} product={product} />
-        ))}
-    </div>
+    <>
+      <div className="w-[80%] max-w-[700px] m-auto mb-[15px]">
+        <input
+          type="search"
+          onChange={onChangeHandler}
+          placeholder="Search products"
+          className="w-[100%] h-[35px] p-[7px] bg-lightgrey rounded-md"
+        />
+      </div>
+      <div className="flex flex-row justify-center flex-wrap max-w-[700px] m-auto">
+        {filteredProducts.length ? (
+          filteredProducts.map((product) => (
+            <ProductCard key={finalProducts.id} product={product} />
+          ))
+        ) : (
+          <span className="text-lg font-medium">No match</span>
+        )}
+      </div>
+    </>
   );
 };
 
